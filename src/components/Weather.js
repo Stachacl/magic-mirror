80eb0def04fba9905525bd25ddddabfcf80692b1
Stachacl <stacha.l@icloud.com>
2022-11-23 10:22:42 +1100

    weather component created

---
+++ src/components/Weather.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactWeather from 'react-open-weather';
+import 'react-open-weather/lib/css/ReactWeather.css';
+
+
+export default function Weather() {
+  return (
+    <div>Weather</div>
+  )
+}
