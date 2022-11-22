077366bf46f2522e6ab253ab6ad2b916f4e0ab42
Stachacl <stacha.l@icloud.com>
2022-11-23 10:54:05 +1100

    weather url checked

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import Clock from "./components/Clock";
 import Day from "./components/Day";
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello, Stacha</p>
-        <Day />
-        <Clock />
+        <div> 
+          <Day />
+          <Clock />
+        </div>
+        <div>
+          <Weather /> 
+        </div>
       </header>
     </div>
   );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import ReactWeather from 'react-open-weather';
-import 'react-open-weather/lib/css/ReactWeather.css';
-
 
 export default function Weather() {
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=-33.8718&lon=151.2002&appid=c34137b24a9b1f78655eb707b6743d49`
   return (
     <div>Weather</div>
   )
