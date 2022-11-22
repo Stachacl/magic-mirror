b82551d9ff43d2220dd71f1529965ecee4cc8699
Stachacl <stacha.l@icloud.com>
2022-11-23 09:38:54 +1100

    added useeffect and set it to update the app evey 1 sec

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Clock() {
+    const [clockState, setClockState] = useState ();
+
+    useEffect(() => {
+        setInterval(() => {
+            const date = new Date();
+            setClockState(date.toLocaleDateString())
+
+        }, 1000) //how often clock should be updated - every 1 second.
+    })
   return (
     <div>Clock</div>
   )
