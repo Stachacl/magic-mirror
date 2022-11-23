204a1954e87f3ba660449c3ca15aca8237eb2b41
Stachacl <stacha.l@icloud.com>
2022-11-23 11:20:56 +1100

    template for temerature and clouds

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,14 @@
 import React from 'react'
+// import data from './Weather-structure'
 
 export default function Weather() {
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=-33.8718&lon=151.2002&appid={myApi}`
+    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=-33.8718&lon=151.2002&appid={myApi}`
+
   return (
-    <div>Weather</div>
+    <div>
+    <p> Weather today: </p>
+    <p> temp , clouds </p>
+    </div>
   )
 }
